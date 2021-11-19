91413bb6e0310d5b6f069bcdffb6310635b8dcce
Devon Long <devonelong@gmail.com>
2021-11-19 15:37:15 -0500

    added validation and moved backend ws to project dir

--- src/app/service/data/todo-data.service.ts
+++ src/app/service/data/todo-data.service.ts
@@ -27,6 +27,11 @@ export class TodoDataService {
   updateTodo(username, id, todo){
     return this.http.put(`http://localhost:8080/users/${username}/todos/${id}`, todo);
   }
+
+  createTodo(username, todo){
+    return this.http.post(`http://localhost:8080/users/${username}/todos`, todo);
+  }
+
 }
 
 
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -21,20 +21,34 @@ export class TodoComponent implements OnInit {
 
   ngOnInit(): void {
     this.id = this.route.snapshot.params['id'];
-    this.todo = new Todo(1, '', false, new Date());
-    this.todoService.retrieveTodo('in28minutes', this.id)
-    .subscribe(
-      data=> this.todo = data
-    )
+    this.todo = new Todo(this.id, '', false, new Date());
+
+    if(this.id!=-1){
+      this.todoService.retrieveTodo('in28minutes', this.id)
+      .subscribe(
+        data=> this.todo = data
+      )
+    }
+    
   }
 
   saveTodo(){
-    this.todoService.updateTodo('in28minutes',this.id, this.todo).subscribe(
-      data=> {
-        console.log(data)
-        this.router.navigate(['todos']);
-      }
-    )
+    if(this.id == -1){
+      this.todoService.createTodo('in28minutes', this.todo).subscribe(
+        data=> {
+          console.log(data)
+          this.router.navigate(['todos']);
+        }
+      )
+    }else{
+      this.todoService.updateTodo('in28minutes',this.id, this.todo).subscribe(
+        data=> {
+          console.log(data)
+          this.router.navigate(['todos']);
+        }
+      )
+    }
+    
 
   }
 
